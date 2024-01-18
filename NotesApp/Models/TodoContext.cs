0cd642b3f5118bfde74919890f793d33254292a9
madhavchandok1 <madhavchandok.iimt@gmail.com>
2024-01-18 17:53:05 +0530

    Initialization of todoApp

---
+++ NotesApp/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotesApp.Models
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; };
+    }
+}
---
+++ NotesApp/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace NotesApp.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
+                                                              
